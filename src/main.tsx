1d547bc7f779c4216f0279a83ce3a9f485a21baf
nmskm240 <utc.am240@gmail.com>
2025-07-19 02:24:37 +0000

    fix Strictmodeを入れると2回描画されるため削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 import { AuthProvider } from "./providers";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>
 );
