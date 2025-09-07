5cade9cdbbbce29da12b088c1a362967ca5da0c1
nmskm240 <utc.am240@gmail.com>
2025-09-07 03:21:33 +0000

    dev Python環境を整備してtypescriptからpythonスクリプトをインポートできるように

--- .devcontainer/on-post-create.sh
+++ .devcontainer/on-post-create.sh
@@ -1,15 +1,22 @@
-# !/bin/sh
+#!/bin/sh
 
+echo "Installing gemini-cli"
 npm install -g @google/gemini-cli
 
 set -e
 
-# tailscaled をバックグラウンドで起動
+echo "Start up tailscaled"
 nohup tailscaled --state=/var/lib/tailscale/tailscaled.state \
   --socket=/var/run/tailscale/tailscaled.sock >/var/log/tailscaled.log 2>&1 &
 
 # デーモンが立ち上がるまでちょっと待つ
 sleep 3
 
-# Tailscale 接続
+echo "Regist tailnet"
 tailscale up --ssh --hostname=${TAILSCALE_DEVICE_NAME}
+
+# Setup Python Virtual Environment
+echo "Creating python virtual environment..."
+python3 -m venv .venv
+echo "Installing python dependencies..."
+.venv/bin/pip install -r .devcontainer/requirements.txt
