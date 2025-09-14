a8f770aa318ba1d0b2190c30c9518ad6bba94e05
nmskm240 <utc.am240@gmail.com>
2025-09-14 13:53:54 +0000

    add favicon追加

---
+++ src/components/AboutButton.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from 'react';
+import { InfoOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { AboutModal } from './AboutModal';
+
+export function AboutButton() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <IconButton color="inherit" onClick={handleClickOpen}>
+        <InfoOutlined />
+      </IconButton>
+      <AboutModal open={open} onClose={handleClose} />
+    </>
+  );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,5 @@ export { Header } from './Header';
 export { FileUploader } from './FileUploader';
 export { SearchBox } from './SearchBox';
 export { SignOutButton } from './SignOutButton';
+export * from './AboutButton';
+export * from './AboutModal';
