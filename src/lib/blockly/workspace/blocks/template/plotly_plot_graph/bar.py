f0e92cb79b742dcd9815312d1bafec7543ba8366
nmskm240 <utc.am240@gmail.com>
2025-09-24 00:27:24 +0000

    fix 不要差分の除去

--- src/lib/blockly/workspace/blocks/template/plotly_plot_graph/bar.py
+++ src/lib/blockly/workspace/blocks/template/plotly_plot_graph/bar.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 
 __BLOCKLY_df__ = pd.DataFrame()
 __BLOCKLY_x__ = ""
@@ -8,8 +7,5 @@ __BLOCKLY_y__ = ""
 __BLOCKLY_title__ = ""
 # --- BLOCKLY TEMPLATE ---
 
-# --- BLOCKLY FUNC ---
-go.Figure.__str__  = lambda self: self.to_json()
-# --- BLOCKLY FUNC END ---
-
-px.bar(__BLOCKLY_df__, x="__BLOCKLY_x__", y="__BLOCKLY_y__", title="__BLOCKLY_title__")
+print(px.bar(__BLOCKLY_df__, x='__BLOCKLY_x__',
+             y='__BLOCKLY_y__', title='__BLOCKLY_title__').to_json())
