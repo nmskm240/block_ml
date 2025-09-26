8447330ecb57a19a5e14282d783d95a81fd644bc
nmskm240 <utc.am240@gmail.com>
2025-09-26 14:26:01 +0000

    feat: DataFrame関連の新しいブロックと機能を追加

--- src/lib/blockly/workspace/blocks/dataframe_isnull.ts
+++ src/lib/blockly/workspace/blocks/dataframe_isnull.ts
@@ -3,25 +3,26 @@ import { Order, pythonGenerator } from 'blockly/python';
 
 import { VariableTypes } from '../types';
 import { applyPlaceholders, createShadowBlock, stripImports } from '../utils';
-import template from './template/dataframe_get_columns.py';
+import template from './template/dataframe_isnull.py';
 
-export const DATAFRAME_GET_COLUMNS = 'dataframe_get_columns';
+export const DATAFRAME_ISNULL = 'dataframe_isnull';
 
-Blockly.Blocks[DATAFRAME_GET_COLUMNS] = {
+Blockly.Blocks[DATAFRAME_ISNULL] = {
   init: function (this: Blockly.Block) {
     this.appendValueInput('df')
       .appendField('DataFrame')
       .setCheck(VariableTypes.Dataframe)
       .setShadowDom(createShadowBlock('variables_get', { VAR: 'df' }));
-    this.appendDummyInput().appendField('の列名');
-    this.setOutput(true, VariableTypes.Array);
+    this.appendDummyInput().appendField('の欠損値を確認');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(210);
-    this.setTooltip('DataFrameのすべての列名をリストとして返します。');
+    this.setTooltip('DataFrameの各列に含まれる欠損値の数を出力します。');
     this.setHelpUrl('');
   },
 };
 
-pythonGenerator.forBlock[DATAFRAME_GET_COLUMNS] = (block, generator) => {
+pythonGenerator.forBlock[DATAFRAME_ISNULL] = (block, generator) => {
   const df = generator.valueToCode(block, 'df', Order.NONE) || 'df';
 
   const body = stripImports(template, generator);
@@ -29,5 +30,5 @@ pythonGenerator.forBlock[DATAFRAME_GET_COLUMNS] = (block, generator) => {
     __BLOCKLY_df__: df,
   });
 
-  return [code, Order.FUNCTION_CALL];
+  return code;
 };
---
+++ src/lib/blockly/workspace/blocks/template/dataframe_dtypes.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+__BLOCKLY_df__ = pd.DataFrame()
+# --- BLOCKLY TEMPLATE ---
+
+print(__BLOCKLY_df__.dtypes)
